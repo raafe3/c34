9b2bcf4f480750ca573348b3c53c1d8e0a3e5fdc
raafe3 <70945202+raafe3@users.noreply.github.com>
2020-12-08 15:58:55 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,44 @@
+var ball;
+var position,database;
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database=firebase.database()
+    var ballposition=database.ref("ball/position")
+    ballposition.on("value",readpos,showerror)
+}
+function readpos(data){
+    position=data.val()
+    ball.x=position.x;
+    ball.y=position.y;
+}
+function showerror(){
+    console.log("could not hit database")
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x : position.x+x,
+        y : position.y+y
+    })
+}
